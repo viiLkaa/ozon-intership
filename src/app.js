bc2a3a24cfe066e4c9a22cac0cb7eefe1b793793
viiLkaa <vgutiyeva@bk.ru>
2026-01-21 13:57:21 +0300

    input fixed

--- src/app.js
+++ src/app.js
@@ -10,13 +10,33 @@ const hideToggle = document.getElementById("hideToggle");
 const progress = new Progress(host, {value:0});
 
 valueInput.addEventListener("input", () => {
+  let value = valueInput.value;
+
+  //убираю все кроме цифр
+  value= value.replace(/[^\d]/g, "");
+
+  if (value ==="")
+  {
+    valueInput.value = "";
+    progress.setValue(0);
+    return;
+  }
+
+  let numericValue = Number(value);
+
+  //ставлю границы от 0 до 100
+  //if (numericValue > 100) numericValue = "none";
+  if (numericValue > 100) numericValue = 100;
+  if (numericValue < 0) numericValue = 0;
+
+  valueInput.value = String(numericValue);
+
   progress.setValue(valueInput.value);
 });
 
 animateToggle.addEventListener("change", () => {
   progress.setAnimated(animateToggle.checked);
 });
-
 hideToggle.addEventListener("change", () => {
   progress.setHidden(hideToggle.checked);
 });
